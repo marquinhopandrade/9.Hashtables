dcd3eb8a87d3ef783de9b01d7e3f605648477f81
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-15 17:31:53 -0300

    Adicionar arquivos de projeto.

---
+++ 9.Hashtables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _9.Hashtables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");            
+        }
+        class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public float GPA { get; set; }
+            public Student(int id, string name, float GPA)
+            {
+                this.Id = id;
+                this.Name = name;
+                this.GPA = GPA;
+            }
+        }
+    }
+}

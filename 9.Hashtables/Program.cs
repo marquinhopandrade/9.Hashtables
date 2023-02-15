e77f8963da8b01d1205f85e3e25b81fa160d10a9
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-15 18:06:41 -0300

    Simplifica a maneira de ler todos os valores da Hashtable sem precisar converter

--- 9.Hashtables/Program.cs
+++ 9.Hashtables/Program.cs
@@ -16,16 +16,24 @@ namespace _9.Hashtables
             studentsTable.Add(stud2.Id, stud2);
             studentsTable.Add(stud3.Id, stud3);
             studentsTable.Add(stud4.Id, stud4);
-            //serve para acessar o id 1 da hash table, porém só funciona pq foi especifica em (Student) que o studentsTable[1] era do tipo objeto (Retrieve individual item with known ID)
+            //(serve para acessar o id 1 da hash table, porém só funciona pq foi especificada em (Student) que o studentsTable[1] era do tipo objeto (Retrieve individual item with known ID)
             Student storedStudent1 = (Student)studentsTable[1];
-            Console.WriteLine("Student ID: {0}, Name:{1}, GPA:{2}", storedStudent1.Id, storedStudent1.Name, storedStudent1.GPA) ;            
+            Console.WriteLine("Student ID: {0}, Name:{1}, GPA:{2}", storedStudent1.Id, storedStudent1.Name, storedStudent1.GPA);
+            Console.WriteLine("///////////////////////////////////////////");
             //retrieve all values from a Hashtable
             foreach (DictionaryEntry entry in studentsTable)
             {
-                Student tempValue = (Student)entry.Value;
+                Student tempValue = (Student)entry.Value;//converte no tipo objeto Student
                 Console.WriteLine("Student ID: {0}", tempValue.Id);
                 Console.WriteLine("Student Name: {0}", tempValue.Name);
                 Console.WriteLine("Student GPA: {0}", tempValue.GPA);
+            }
+            foreach (Student value in studentsTable.Values)//.Values permite que você acesse o valor diretamente sem precisar converter os valores em objeto aluno(como na linha 26)
+            {
+                Console.WriteLine("Student ID: {0}", value.Id);
+                Console.WriteLine("Student Name: {0}", value.Name);
+                Console.WriteLine("Student GPA: {0}", value.GPA);
+                //o mesmo resultado só que sem precisar converter
             }
         }
         class Student

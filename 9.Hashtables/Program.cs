5daa7fca94f70b7961c9a33cc2a93029de8a01a9
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-15 17:56:29 -0300

    Implementa maneiras de recuperar valores em uma Hashtable

--- 9.Hashtables/Program.cs
+++ 9.Hashtables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;//necessário para usar hashtables
 
 namespace _9.Hashtables
 {
@@ -6,7 +7,26 @@ namespace _9.Hashtables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");            
+            Hashtable studentsTable = new Hashtable();
+            Student stud1 = new Student(1, "Maria", 98);
+            Student stud2 = new Student(2, "Jason", 76);
+            Student stud3 = new Student(3, "Clara", 43);
+            Student stud4 = new Student(4, "Steve", 55);
+            studentsTable.Add(stud1.Id, stud1);
+            studentsTable.Add(stud2.Id, stud2);
+            studentsTable.Add(stud3.Id, stud3);
+            studentsTable.Add(stud4.Id, stud4);
+            //serve para acessar o id 1 da hash table, porém só funciona pq foi especifica em (Student) que o studentsTable[1] era do tipo objeto (Retrieve individual item with known ID)
+            Student storedStudent1 = (Student)studentsTable[1];
+            Console.WriteLine("Student ID: {0}, Name:{1}, GPA:{2}", storedStudent1.Id, storedStudent1.Name, storedStudent1.GPA) ;            
+            //retrieve all values from a Hashtable
+            foreach (DictionaryEntry entry in studentsTable)
+            {
+                Student tempValue = (Student)entry.Value;
+                Console.WriteLine("Student ID: {0}", tempValue.Id);
+                Console.WriteLine("Student Name: {0}", tempValue.Name);
+                Console.WriteLine("Student GPA: {0}", tempValue.GPA);
+            }
         }
         class Student
         {
